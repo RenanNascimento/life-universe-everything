6015b8f2ad8c8c85b209ea60754ac0de10059ede
renannascimento <renanmateusbn@gmail.com>
2019-03-05 12:34:53 -0300

    Adding calculator-tricks-1 challenge

---
+++ 001-calculator-tricks-1/calculator-tricks-1.js
@@ -0,0 +1,24 @@
+const sumByNum = x => y => x + y;
+const multiplyByNum = x => y => x * y;
+const powerToNum = x => y => x**y; 
+const repeat = n => f => x =>
+  n === 0 ? x : repeat (n - 1) (f) (f(x - 1))
+
+const sumBy13 = sumByNum(13);
+const multiplyBy8 = multiplyByNum(8);
+const tenToPower = powerToNum(10);
+const eightFollowedByNZeros = x => multiplyBy8(tenToPower(x))
+const numOnlyEightDigits = x => repeat(x)(acc => acc + eightFollowedByNZeros(x))(0)
+
+
+
+/*const numOnlyEightDigits = x => {
+    let acc = 0;
+    x = x - 1;
+    while(x >= 0){
+        acc += eightFollowedByNZeros(x--)
+    }
+    return acc;
+}*/
+
+console.log(numOnlyEightDigits(1))
