305d313df3db53fa8da4da690c25259d619eb97c
Renan Mateus Bernardo do Nascimento <renan.nascimento@ibm.com>
2019-03-07 16:54:38 -0300

    Creating loop

--- 001-calculator-curiosity-1/calculator-curiosity-1.js
+++ 001-calculator-curiosity-1/calculator-curiosity-1.js
@@ -1,8 +1,9 @@
 const sumByNum = x => y => x + y;
 const multiplyByNum = x => y => x * y;
-const powerToNum = x => y => x**y; 
+const powerToNum = x => y => x ** y;
 const repeat = n => f => x =>
-  n === 0 ? x : repeat (n - 1)(f)(x + f(n - 1))
+  n === 0 ? x : repeat(n - 1)(f)(x + f(n - 1))
+
 
 const sumBy13 = sumByNum(13);
 const multiplyBy8 = multiplyByNum(8);
@@ -10,4 +11,9 @@ const tenToPower = powerToNum(10);
 const eightFollowedByNZeros = x => multiplyBy8(tenToPower(x))
 const numOnlyDigitsEight = x => repeat(x)(acc => eightFollowedByNZeros(acc))(eightFollowedByNZeros(x))
 
-console.log(sumBy13(multiplyBy8(numOnlyDigitsEight(2))))
+const loopRange = 10;
+let i = 1;
+while (i <= 10) {
+  console.log(sumBy13(multiplyBy8(numOnlyDigitsEight(i))));
+  i = i + 1;
+}
