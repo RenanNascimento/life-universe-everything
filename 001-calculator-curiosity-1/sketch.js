d79ea27fa9a913f015f302dca7e23ea2d5a71104
renannascimento <renanmateusbn@gmail.com>
2019-03-09 17:23:21 -0300

    Including slider to iterate thourgh the series

--- 001-calculator-curiosity-1/sketch.js
+++ 001-calculator-curiosity-1/sketch.js
@@ -1,16 +1,22 @@
-const sizeRange = 10;
+const initSliderRange = 0;
+const endSliderRange = 15;
+const xSlider = 20;
+const ySlider = xSlider;
+const xSliderText = 155;
+const ySliderText = 65;
+const xSeriesOffset = 35;
+const numSpacing = 50;
 
-let font,
-  fontsize = 40;
+let intSlider;
+let font, fontsize = 40;
 
 function preload() {
   font = loadFont('assets/SourceSansPro-Regular.otf');
 }
 
 function setup() {
+  // Create canvas
   createCanvas(window.innerWidth, window.innerHeight);
-  background(0);
-
   strokeWeight(1);
   stroke(255);
 
@@ -19,12 +25,21 @@ function setup() {
   textSize(fontsize);
   textAlign(CENTER, CENTER);
 
- 
+  // Create slider
+  intSlider = createSlider(initSliderRange, endSliderRange, initSliderRange);
+  intSlider.position(xSlider, ySlider);
+}
+
+function draw() {
+  background(0);
+  let i = intSlider.value();
+
+  text('Number of 1\'s: '+i, xSliderText, ySliderText);
+
   fill(255);
-  let num = sumBy13(multiplyBy8(numOnlyDigitsEight(sizeRange)))+'';
+  let num = sumBy13(multiplyBy8(numOnlyDigitsEight(i)))+'';
   let digits = num.match(/\d/g);
   digits.forEach((d, index) => {
-    text(char(d.charCodeAt(0)), index*100+100, window.innerHeight/2);
+    text(char(d.charCodeAt(0)), index*numSpacing+xSeriesOffset, window.innerHeight/2);
   });
-  
 }
