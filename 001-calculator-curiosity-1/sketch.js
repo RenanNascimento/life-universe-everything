e6a03c05e500698e232d4b7adf5c03e38b26a9dd
renannascimento <renanmateusbn@gmail.com>
2019-03-08 23:14:57 -0300

    Adding letters

--- 001-calculator-curiosity-1/sketch.js
+++ 001-calculator-curiosity-1/sketch.js
@@ -1,12 +1,30 @@
+let font,
+  fontsize = 40;
+
+function preload() {
+  font = loadFont('assets/SourceSansPro-Regular.otf');
+}
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   background(0);
 
   strokeWeight(5);
   stroke(255);
+
+  // Set text characteristics
+  textFont(font);
+  textSize(fontsize);
+  textAlign(CENTER, CENTER);
+
 }
 
 function draw() {
   fill(0);
   ellipse(200, 200, 80, 80);
+
+  fill(255);
+  let letter = char(48);
+  text(letter, 200, 200);
+
 }
